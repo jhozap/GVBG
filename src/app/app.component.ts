da91f21ddcf1155167c10b60ca5eec1c75230363
Jhonny Zapata <jhonny.z1022@gmail.com>
2020-02-12 22:02:22 -0500

    implementacion inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,41 +9,45 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Inicio',
+      url: '/home',
+      icon: 'home'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Glosario',
+      url: '/glosario',
+      icon: 'help'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
+      title: 'Violencia basada en género',
+      url: '/violencia-basada-genero',
+      icon: 'woman'
     },
     {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
+      title: 'Marco juridico',
+      url: '/marco-juridico',
+      icon: 'book'
     },
     {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
+      title: 'Ataques con agentes quimicos',
+      url: '/ataques-agentes-quimicos',
+      icon: 'nuclear'
     },
     {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'Rutas institucionales',
+      url: '/rutas-institucionales',
+      icon: 'walk'
+    },
+    {
+      title: 'Creditos',
+      url: '/creditos',
+      icon: 'newspaper'
     }
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  ];  
 
   constructor(
     private platform: Platform,
@@ -58,12 +62,5 @@ export class AppComponent implements OnInit {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
-  }
-
-  ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
-  }
+  }  
 }
