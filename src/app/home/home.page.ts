ab520e52661993fafb8927996709906e0bf77be8
Jhonny Zapata <jhonny.z1022@gmail.com>
2020-02-13 21:34:41 -0500

    glosario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  slides = [
+    {
+      src: 'assets/img/1.png',
+      alt: 'Slide 1'
+    },
+    {
+      src: 'assets/img/2.png',
+      alt: 'Slide 2'
+    },
+    {
+      src: 'assets/img/3.png',
+      alt: 'Slide 3'
+    },
+    {
+      src: 'assets/img/4.png',
+      alt: 'Slide 4'
+    },
+    {
+      src: 'assets/img/5.png',
+      alt: 'Slide 5'
+    }
+  ];
+
   slideOpts = {
     initialSlide: 0,
     speed: 400,
