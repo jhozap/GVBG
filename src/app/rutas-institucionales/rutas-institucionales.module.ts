e52e3838d952a57b1dfefd40cc72762acfcb4d45
Jhonny Zapata <jhonny.z1022@gmail.com>
2020-02-14 13:17:50 -0500

    rutas institucionales

--- src/app/rutas-institucionales/rutas-institucionales.module.ts
+++ src/app/rutas-institucionales/rutas-institucionales.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RutasInstitucionalesPageRoutingModule } from './rutas-institucionales-routing.module';
 
@@ -14,8 +13,7 @@ import { RutasInstitucionalesPage } from './rutas-institucionales.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RutasInstitucionalesPageRoutingModule,
-    BrowserAnimationsModule
+    RutasInstitucionalesPageRoutingModule
   ],
   declarations: [RutasInstitucionalesPage]
 })
--- src/app/rutas-institucionales/rutas-institucionales.page.ts
+++ src/app/rutas-institucionales/rutas-institucionales.page.ts
@@ -1,43 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  query,
-  animateChild
-} from '@angular/animations';
 
 @Component({
   selector: 'app-rutas-institucionales',
   templateUrl: './rutas-institucionales.page.html',
-  styleUrls: ['./rutas-institucionales.page.scss'],
-  animations: [
-    trigger('ngIfAnimation', [
-      transition(':enter, :leave', [
-        query('@*', animateChild())
-      ])
-    ]),
-    trigger('easeInOut', [
-      transition('void => *', [
-          style({
-              opacity: 0
-          }),
-          animate("500ms ease-in", style({
-              opacity: 1
-          }))
-      ]),
-      transition('* => void', [
-          style({
-              opacity: 1
-          }),
-          animate("500ms ease-in", style({
-              opacity: 0
-          }))
-        ])
-      ])
-  ]
+  styleUrls: ['./rutas-institucionales.page.scss']  
 })
 export class RutasInstitucionalesPage implements OnInit {
 
