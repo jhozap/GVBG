c999c5318221a73c919f3f73e03579f37eb1e2b8
Jhonny Zapata <jhonny.z1022@gmail.com>
2020-02-17 23:32:56 -0500

    ajuste de interfaz usuario

--- src/app/modal-ruta-two/modal-ruta-two.page.ts
+++ src/app/modal-ruta-two/modal-ruta-two.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-modal-ruta-two',
@@ -8,21 +9,49 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalRutaTwoPage implements OnInit {
 
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController, public alertController: AlertController) { }
 
   selected = 0;
+  alerta = '<ol> \
+  <li> \
+    Si la víctima presenta lesiones o abuso sexual debe trasladarse de manera \
+    inmediata al centro de salud más cercano, sin perder la localización de la \
+    víctima, para que se active el Protocolo de Atención Integral en Salud \
+    para Víctimas creado por el Ministerio de Salud y Protección Social. \
+  </li> \
+  <br> \
+  <li> \
+    La víctima no deberá ser confrontada en ningún MOMENTO con el victimario. \
+  </li> \
+  <br> \
+  <li> \
+    En caso de que la víctima sea recluida en un centro de salud, el \
+    uniformado que conoció el caso deberá coordinar con la Policía Judicial la \
+    recepción de la denuncia. \
+  </li> \
+</ol>';
 
   ngOnInit() {
   }
 
   toggle(index) {
-    $(".forma-" + this.selected).toggle("slow");
+    $(".content-" + this.selected).toggle("slow");
     if (index != this.selected) {
-      $(".forma-" + index).toggle("slow");
+      $(".content-" + index).toggle("slow");
       this.selected = index;
     } else {
       this.selected = 0;
     }
+  }
+
+  async presentAlert(i) {
+    const alert = await this.alertController.create({
+      header: "Para tener en cuenta",
+      message: this.alerta,
+      buttons: ["OK"]
+    });
+
+    await alert.present();
   }
 
   close() {
